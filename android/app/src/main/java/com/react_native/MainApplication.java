0ab4eb3f3c8d675df30d6ef824381d570f63b2df
Charmian Cheng (Beyondsoft Corporation) <v-chmin@microsoft.com>
2017-08-15 14:23:03 +0800

    upload

--- android/app/src/main/java/com/react_native/MainApplication.java
+++ android/app/src/main/java/com/react_native/MainApplication.java
@@ -3,10 +3,15 @@ package com.react_native;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.azure.mobile.react.push.RNPushPackage;
+import com.microsoft.azure.mobile.react.crashes.RNCrashesPackage;
+import com.microsoft.azure.mobile.react.analytics.RNAnalyticsPackage;
+import com.microsoft.azure.mobile.react.mobilecenter.RNMobileCenterPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.microsoft.azure.mobile.MobileCenter;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNPushPackage(MainApplication.this),
+            new RNCrashesPackage(MainApplication.this, getResources().getString(R.string.mobileCenterCrashes_whenToSendCrashes)),
+            new RNAnalyticsPackage(MainApplication.this, getResources().getString(R.string.mobileCenterAnalytics_whenToEnableAnalytics)),
+            new RNMobileCenterPackage(MainApplication.this)
       );
     }
   };
@@ -34,6 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public void onCreate() {
+    MobileCenter.setLogUrl("https://in-staging-south-centralus.staging.avalanch.es");
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
   }
--- index.android.js
+++ index.android.js
@@ -5,32 +5,101 @@
  */
 
 import React, { Component } from 'react';
+import MobileCenter from 'mobile-center';
+import Analytics from "mobile-center-analytics";
+import Crashes from "mobile-center-crashes";
+import Push from 'mobile-center-push';
 import {
+  AppState,
+  Button,
+  Alert,
   AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+async function ClickBtn() {  
+  const installId = await MobileCenter.getInstallId();
+  alert(installId); 
 
+}
+function ClickBtn1() {  
+  alert("Welcome to coolest app ever"); 
+
+}
+  function ChangeDisable() {  
+    this.setState({  
+      disabled: this.state.disabled ? false : true  
+    }); 
+    throw new Error("This is a test javascript crash!"); 
+}  
 export default class React_Native extends Component {
+    constructor(props){        
+    super(props);  
+    this.state = {  
+      disabled : false,  
+    }  
+  }
   render() {
+     Analytics.trackEvent("Learn More");
+     Analytics.trackEvent("Change");
+     Analytics.trackEvent("OK");
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+       <Button  
+        onPress={ClickBtn1}
+        title="Login"  
+        color="red"  
+        accessibilityLabel="Learn more about this purple button" 
+         /> 
+        <Button  
+        onPress={ClickBtn}  
+        title="Learn More"  
+        color="green"  
+        disabled={this.state.disabled}  
+        accessibilityLabel="Learn more about this purple button" 
+        /> 
+        <Button  
+        onPress={ChangeDisable.bind(this)}  
+        title="Change"  
+        color="gray"  
+        accessibilityLabel="Learn more about this purple button"
+        /> 
       </View>
     );
   }
 }
+Push.setEventListener({
+  pushNotificationReceived: function (pushNotification) {
+    let message = pushNotification.message;
+    let title = pushNotification.title;
 
+    if (message === null || message === undefined) {
+      // Android messages received in the background don't include a message. On Android, that fact can be used to
+      // check if the message was received in the background or foreground. For iOS the message is always present.
+      title = "Android background";
+      message = "<empty>";
+    }
+
+    // Custom name/value pairs set in the Mobile Center web portal are in customProperties
+    if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+      message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+    }
+
+    if (AppState.currentState === 'active') {
+      Alert.alert(title, message);
+    }
+    else {
+      // Sometimes the push callback is received shortly before the app is fully active in the foreground.
+      // In this case you'll want to save off the notification info and wait until the app is fully shown
+      // in the foreground before displaying any UI. You could use AppState.addEventListener to be notified
+      // when the app is fully in the foreground.
+    }
+  }
+});
 const styles = StyleSheet.create({
   container: {
     flex: 1,
